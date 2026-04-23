868c35e5e4b6ac9dfc2b2d537aec3f5fbb6716ce
Chen Guoyi <chenguoyi@u.nus.edu>
2026-04-23 19:54:43 +0800

    Update SECRET_KEY in Config class

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'CgyLoveChina'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'CgyLoveNUSNightRunners'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///bingo.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
